094636fcad7c8dca0511efd2dd39df0c8368d392
Vincent-Lin-UF <vincentlin1617@gmail.com>
2024-10-25 04:25:12 -0400

    added AWS beanstalk

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: 'https://siege-swe.vercel.app/' }));
 
 const apiRoutes = require('./routes');
 app.use('/api', apiRoutes);
