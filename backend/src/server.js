9663203910c85d6c7e7c8169f969ee845f0184fa
Vincent-Lin-UF <vincentlin1617@gmail.com>
2024-10-25 04:45:46 -0400

    changed port

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI)
