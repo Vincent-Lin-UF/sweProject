3e5a9b30de7d813e138db29971eebc2e171dacc9
Vincent-Lin-UF <vincentlin1617@gmail.com>
2024-10-25 04:50:18 -0400

    removed mongoose

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,16 +4,4 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const PORT = process.env.PORT || 8080;
-const MONGO_URI = process.env.MONGO_URI;
-
-mongoose.connect(MONGO_URI)
-    .then(() => {
-        console.log('Connected to the Mango🥭');
-        app.listen(PORT, () => {
-            console.log(`Server be running on ${PORT} no CAP 🙅`);
-        });
-    })
-    .catch((error) => {
-        console.error('Oopsie daisy seems like Mango be issuing 😭:', error);
-});
+const PORT = 8080;
