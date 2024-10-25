68b1ba386e94d1d6e102e2952bcad9d94245b42d
Vincent-Lin-UF <vincentlin1617@gmail.com>
2024-10-25 05:13:48 -0400

    changed deployment url

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(cors({ origin: 'https://siege-swe.vercel.app' }));
+app.use(cors({ origin: 'https://siege-n3tpz1f2p-vincentlinufs-projects.vercel.app' }));
 app.use(express.json());
 
 const apiRoutes = require('./routes');
