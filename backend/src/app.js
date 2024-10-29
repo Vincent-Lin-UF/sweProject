fce41a54f3830d9e69aa575f373709a3e26fc863
anthony <anthonyjyao@gmail.com>
2024-10-28 20:36:55 -0400

    f

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const cors = require('cors');
+const loginJS = require('login-express');
 
 const app = express();
+
+const dbConfig = {
+  mongodbURI: process.env.MONGO_URI, // required
+  jwtSecret: process.env.JWT_SECRET, // required
+  passwordLength: 10, // default: dev8
+  jwtSessionExpiration: 3600 // default: 7200
+};
+
+const appConfig = {
+  jwtResetSecret: process.env.JWT_RESET_SECRET, // required
+  emailFromUser: 'myemail@example.com', // required
+  emailFromPass: 'myemailpassword', // required
+  emailHost: 'stmp.myemailserver.com', // required
+  emailPort: 465, // required
+  emailSecure: true, // required
+  jwtResetExpiration: 1000, // default: 900
+  basePath: '/auth' // default: '/api'
+};
+
+loginJS(dbConfig, appConfig, app, express);
 
 const allowedOrigins = [
   'https://siege-swe.vercel.app',  
