cd3b8ed213007af0706097438e6a335928f3d34e
Vincent-Lin-UF <vincentlin1617@gmail.com>
2024-11-27 05:39:33 -0500

    Added Andy's Hero component into a page and changed favicon and tab name

--- frontend/src/routes/Root.js
+++ frontend/src/routes/Root.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import { AppBar, Toolbar, Typography, Container, Box } from '@mui/material';
+import SiegeLogo from '../assets/siege-logo.png';
 
 function Root() {
     return (
@@ -15,20 +16,21 @@ function Root() {
                         sx={{ flexGrow: 1 }} 
                         style={{ color: "#292729" }}
                     >
-                    <Link to="/" style={{ textDecoration: 'none', color: "#292729" }}>
-                        Siege
+                    <Link to="/" style={{ textDecoration: 'none', color: '#292729', display: 'flex', alignItems: 'center' }}>
+                        <img 
+                            src={SiegeLogo} 
+                            alt="Siege Logo" 
+                            style={{ height: '40px', marginRight: '10px' }} // Adjust height and spacing as needed
+                        />
                     </Link>
                     </Typography>
-                    <Link to="/" style={{ color: '#292729', textDecoration: 'none', margin: '0 10px' }}>
-                        Home
-                    </Link>
                     <Link to="/login" style={{ color: '#292729', textDecoration: 'none', margin: '0 10px' }}>
                         Login
                     </Link>
                     <Link to="/dashboard" style={{ color: '#292729', textDecoration: 'none', margin: '0 10px' }}>
                         Dashboard
                     </Link>
-                </Toolbar>
+                </Toolbar>  
             </AppBar>
 
             {/* Main Content */}
